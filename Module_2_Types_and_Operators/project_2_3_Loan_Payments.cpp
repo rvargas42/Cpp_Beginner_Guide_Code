a458e56b432569e44aa7c71242e9e91844043684
rvargas42 <ravargas.42t@gmail.com>
2024-06-05 15:15:21 +0200

    loan payments

--- Module_2_Types_and_Operators/project_2_3_Loan_Payments.cpp
+++ Module_2_Types_and_Operators/project_2_3_Loan_Payments.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 int main(void)
 {
@@ -6,4 +7,24 @@ int main(void)
     unsigned int    principal;
     double          paymentsYear;
     double          time;
+    double          num, den;
+    double          payment;
+    double          b, e;
+
+    std::cout << "Enter the principal amount";
+    std::cin >> principal;
+    std::cout << "Interest Rate: ";
+    std::cin >> r;
+    std::cout << "Payments per year: ";
+    std::cin >> paymentsYear;
+    std::cout << "number of years: ";
+    std::cin >> time;
+
+    num = r * (principal / paymentsYear);
+    b = 1 - ((r / paymentsYear) + 1);
+    e = -(paymentsYear * time);
+    den = std::pow(b,e);
+    payment = num / den;
+
+    std::cout << "Payment is: " << payment;
 }
